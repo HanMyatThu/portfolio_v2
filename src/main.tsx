add523e143c6782fde5ba9a4fa1c28e2d512168f
Han Myat Thu <hanmyatthu111@gmail.com>
2026-04-05 01:29:00 +0700

    add tailwind & lenis

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import Lenis from "lenis";
+import AnimatedCursor from "react-animated-cursor";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const lenis = new Lenis();
+
+function raf(time: number) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AnimatedCursor
+      innerSize={8}
+      outerSize={35}
+      color="14,165,233"
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={2}
+    />
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  title: string;
+  desc: string;
+}
+
+export interface ExperienceItem {
+  role: string;
+  company: string;
+  year: string;
+}
